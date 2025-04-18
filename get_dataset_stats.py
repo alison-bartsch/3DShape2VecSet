fixed global point cloud normalization for improved consistency

get_dataset_stats.py
@@ -1,5 +1,40 @@
 import numpy as np
+from tqdm import tqdm
 
-# TODO: iterate through the trajectories, load in the point clouds, and record the minimum and maximum x,y,z
-# append the min/max to a list 
+traj_list = [('/Trajectory0', 33), 
+            ('/Trajectory1', 27), 
+            ('/Trajectory2', 26), 
+            ('/Trajectory3', 26), 
+            ('/Trajectory4', 17), 
+            ('/Trajectory5', 23)]
+
+data_path = '/home/alison/Documents/Feb26_Human_Demos_Raw/pottery'
+
+minx = []
+miny = []
+minz = []
+maxx = []
+maxy = []
+maxz = []
+
+for traj in tqdm(traj_list):
+    traj_path = data_path + traj[0]
+    for i in range(traj[1]):
+        # load in the point cloud
+        pc = np.load(traj_path + f'/unnormalized_pointcloud{i}.npy')
+        pc = pc - np.mean(pc, axis=0)
+        # get the min/max x,y,z
+        minx.append(np.min(pc[:, 0]))
+        miny.append(np.min(pc[:, 1]))
+        minz.append(np.min(pc[:, 2]))
+        maxx.append(np.max(pc[:, 0]))
+        maxy.append(np.max(pc[:, 1]))
+        maxz.append(np.max(pc[:, 2]))
+
 # calculate the global min/max from the list
+print("minx: ", np.min(minx))
+print("miny: ", np.min(miny))
+print("minz: ", np.min(minz))
+print("maxx: ", np.max(maxx))
+print("maxy: ", np.max(maxy))
+print("maxz: ", np.max(maxz))

real_world_dataset.py
@@ -142,6 +142,14 @@ class SubGoalDataset:
         state = R @ state.T
         pcl_aug = state.T + center
         return pcl_aug
+    
+    def _normalize_pcl(self, pcl):
+        min_pos = np.array([-0.75, -0.75, -0.75])
+        max_pos = np.array([0.75, 0.75, 0.75])
+        # Normalize the point cloud to be between -1 and 1
+        pcl = (pcl - min_pos) / (max_pos - min_pos)
+        pcl = 2 * pcl - 1
+        return pcl
 
     def __len__(self):
         return self.n_datapoints
@@ -171,10 +179,13 @@ class SubGoalDataset:
         next_state -= center
         goal -= center
 
-        # normalize the state point clouds to be between -1 and 1
-        state = state / np.max(np.abs(state))
-        next_state = next_state / np.max(np.abs(next_state))
-        goal = goal / np.max(np.abs(goal))
+        # # normalize the state point clouds to be between -1 and 1
+        # state = state / np.max(np.abs(state))
+        # next_state = next_state / np.max(np.abs(next_state))
+        # goal = goal / np.max(np.abs(goal))
+        state = self._normalize_pcl(state)
+        next_state = self._normalize_pcl(next_state)
+        goal = self._normalize_pcl(goal)
 
         # convert to torch tensors of correct shape/format
         state = torch.from_numpy(state).float()